eda7cbb1bcf39ac4f386d55c17505eb7c23c8cd1
sisi0318 <gamemode12345@163.com>
2024-10-05 09:18:11 +0800

    再改

--- GetAppid.py
+++ GetAppid.py
@@ -4,9 +4,24 @@ from time import sleep
 import frida
 import requests
 
+def send_key(msg):
+    env = dict(os.environ)
+    print(env)
+    api = os.getenv('API')
+    key = os.getenv('SEND_KEY')
+    group = os.environ['SEND']
+    data = json.dumps({
+        'group_id': group,
+        'message': "action appid\n"+msg
+    })
+    headers = {
+        'Content-Type': "application/json",
+        'Authorization': "Bearer "+ key
+    }
+    r = requests.post(api+"/send_group_msg", headers=headers, data=data)
+
 def on_message(message, data):
     if message['type'] == 'send':
-        key = os.getenv('SEND_KEY')
         url = os.environ['QQURL']
         array ="[URL] "+ url + "\n[Appid] "+message['payload']
 
@@ -36,20 +51,6 @@ def main():
     print("[!] Ctrl+D on UNIX, Ctrl+Z on Windows/cmd.exe to detach from instrumented program.\n\n")
     sleep(30)
     session.detach()
-
-def send_key(msg):
-    api = os.getenv('API')
-    key = os.getenv('SEND_KEY')
-    group = os.environ['SEND']
-    data = json.dumps({
-        'group_id': group,
-        'message': "action appid\n"+msg
-    })
-    headers = {
-        'Content-Type': "application/json",
-        'Authorization': "Bearer "+ key
-    }
-    r = requests.post(api+"/send_group_msg", headers=headers, data=data)
 
 
 if __name__ == '__main__':
